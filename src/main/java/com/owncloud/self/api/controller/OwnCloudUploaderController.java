a2c9442eed8ab215331baa3d63956fd7c71e5f7e
joseleon1903 <joseleon1903@outlook.com>
2026-01-19 23:57:50 -0400

    add code view file video

--- src/main/java/com/owncloud/self/api/controller/OwnCloudUploaderController.java
+++ src/main/java/com/owncloud/self/api/controller/OwnCloudUploaderController.java
@@ -1,17 +1,16 @@
 package com.owncloud.self.api.controller;
 
 import com.owncloud.self.api.service.OwnCloudService;
+import com.owncloud.self.api.utils.LimitedInputStream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Collections;
 import java.util.List;
@@ -111,5 +110,42 @@ public class OwnCloudUploaderController {
                         "inline; filename="+resource.getFilename())
                 .build();
     }
+
+    @GetMapping("/video")
+    public ResponseEntity<Resource> streamVideo(@RequestParam(value = "root", defaultValue = "") String root,
+                                                @RequestHeader HttpHeaders headers) throws Exception {
+
+        Resource video = ownCloudService.getFile(root);
+        long contentLength = video.contentLength();
+
+        List<HttpRange> ranges = headers.getRange();
+
+        if (ranges.isEmpty()) {
+            return ResponseEntity.ok()
+                    .contentType(MediaType.valueOf("video/mp4"))
+                    .contentLength(contentLength)
+                    .body(video);
+        }
+
+        HttpRange range = ranges.get(0);
+        long start = range.getRangeStart(contentLength);
+        long end = range.getRangeEnd(contentLength);
+        long rangeLength = end - start + 1;
+
+        InputStream inputStream = video.getInputStream();
+        inputStream.skip(start);
+
+        Resource region = new InputStreamResource(
+                new LimitedInputStream(inputStream, rangeLength)
+        );
+
+        return ResponseEntity.status(HttpStatus.PARTIAL_CONTENT)
+                .contentType(MediaType.valueOf("video/mp4"))
+                .header(HttpHeaders.CONTENT_RANGE,
+                        "bytes " + start + "-" + end + "/" + contentLength)
+                .contentLength(rangeLength)
+                .body(region);
+    }
+
 
 }
